2e9121a9f4b503286d103fcf8dfa638f4c1cf0fb
Pandelofitness <111893411+Pandelofitness@users.noreply.github.com>
2023-08-10 00:55:27 -0300

    Add files via upload

--- funciones.js
+++ funciones.js
@@ -1,7 +1,10 @@
 boton = document.querySelector(".hamburger");
 mobile = document.querySelector(".div_mobile")
 
+
+
 boton.addEventListener("click",()=>{
     mobile.classList.toggle("div_mobiledespleged")
 
 })
+
